93fb522c3752839d7345356fdfa68467b2e4505f
Santiago Vivas <60908806+sagov8@users.noreply.github.com>
2021-07-20 13:18:56 -0500

    add forms and navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ListCitasComponent } from './components/list-citas/list-citas.component';
 import { CrearCitaComponent } from './components/crear-cita/crear-cita.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ListCitasComponent,
-    CrearCitaComponent
+    CrearCitaComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
